9719ab0d2baeee517ac58ca7b1a2bfeb84bbf288
NORA-NAO <eda.ynora@gmail.com>
2020-05-22 20:31:33 -0500

    Jueguito

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Ping Pong 
+	</title>
+	<script type="text/javascript"src="js/Math.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/Style.css">
+	<meta charset="utf-8">
+</head>
+<body style="background-color:#E8B299">
+<div class="Titulo">
+	<font size="12" style="font-family:Tahoma" style="color: ">Ping Pong  Facilito</font> 
+</div>
+
+<canvas id="canvas" style="border: solid">	
+</canvas>
+
+<br>
+<br>
+ <div>
+<font style="font-family: Bahnschrift Condensed" style="color:#8C472E" size="6">
+	<b>CONTROLES</b>
+</font>
+<br>
+<br>
+<font style="font-family: Bahnschrift Condensed" style="color:#8C472E" size="4">
+	Iniciar-Pausar =Tecla Espacio 
+</font>
+<br>
+<font style="font-family: Bahnschrift Condensed" style="color:#8C472E" size="4">
+	 Barra Derecha= <img src="img/flechas.svg"  width="20" height="20">  
+</font>
+<br>
+<font style="font-family: Bahnschrift Condensed" style="color:#8C472E" size="4">
+	Barra Izquierda = w,s
+</font>
+</div>
+
+</body>
+</html>
